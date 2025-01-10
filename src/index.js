bc07d7c5cbf9050af469f7e7459ccb8c76cac1f0
lajor <rojal.lajor@gmail.com>
2025-01-10 20:09:27 +0545

    removed unnecessay code from root

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "layouts/Admin.js";
+import Auth from "layouts/Auth.js";
+
+// views without layouts
+
+import Index from "views/Index.js";
+import Landing from "views/Landing.js";
+import Profile from "views/Profile.js";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Switch>
+      {/* add routes with layouts */}
+      <Route path="/admin" component={Admin} />
+      <Route path="/auth" component={Auth} />
+      {/* add routes without layouts */}
+      <Route path="/landing" exact component={Landing} />
+      <Route path="/profile" exact component={Profile} />
+      <Route path="/" exact component={Index} />
+      {/* add redirect for first page */}
+      <Redirect from="*" to="/" />
+    </Switch>
+  </BrowserRouter>
+);
